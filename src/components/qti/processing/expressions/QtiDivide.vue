58c015dc752365ab27ad602362b3fb7ab7605827
paulgrudnitski <paul.grudnitski@gmail.com>
2024-06-13 11:12:40 -0700

    Migrate QtiDivide

--- src/components/qti/processing/expressions/QtiDivide.vue
+++ src/components/qti/processing/expressions/QtiDivide.vue
@@ -15,11 +15,13 @@
  * positive and negative infinity) then the operator should result in NULL.
  */
 import QtiValidationException from '@/components/qti/exceptions/QtiValidationException'
+import QtiAttributeValidation from '@/components/qti/validation/QtiAttributeValidation'
 import QtiEvaluationException from '@/components/qti/exceptions/QtiEvaluationException'
 import QtiExitProcessingException from '@/components/qti/exceptions/QtiExitProcessingException'
 import QtiProcessing from '@/components/qti/processing/utils/QtiProcessing'
 import BigNumber from 'bignumber.js'
 
+const qtiAttributeValidation = new QtiAttributeValidation()
 const qtiProcessing = new QtiProcessing()
 
 export default {
@@ -57,18 +59,30 @@ export default {
       return this.valueCardinality
     },
 
-    isValidSlot (slot) {
-      if (typeof slot.componentOptions !== 'undefined') {
-        return true
-      } else {
-        // check if text is something not empty
-        if ((typeof slot.text !== 'undefined') && (slot.text.trim().length > 0)) {
-          // not an empty text slot.  this is an error.
-          throw new QtiValidationException('Invalid Child Node: "' + slot.text.trim() + '"')
-        } else {
-          // empty text slot.  not a component, but not an error
-          return false
+    /**
+     * Validate the child nodes:
+     * expressions (2)
+     */
+    validateChildren: function () {
+      let countExpressions = 0
+
+      if (!this.$slots.default) {
+        throw new QtiValidationException('Must have two Expression nodes')
+      }
+
+      this.$slots.default().forEach((slot) => {
+        if (qtiAttributeValidation.isValidSlot(slot)) {
+          // Detect an expression
+          if (qtiProcessing.isExpressionNode(qtiAttributeValidation.kebabCase(slot.type.name))) {
+            countExpressions += 1
+          } else {
+            throw new QtiValidationException('Node is not an Expression: "' + slot.type.name + '"')
+          }
         }
+      })
+
+      if (countExpressions !== 2) {
+        throw new QtiValidationException('Must have exactly two Expression nodes')
       }
     },
 
@@ -76,41 +90,15 @@ export default {
      * Iterate through the child nodes:
      * expressions (2)
      */
-    validateChildren: function () {
-      let countExpression = 0
-      this.$slots.default.forEach((slot) => {
-        if (this.isValidSlot(slot)) {
-          // Detect an expression
-          if (qtiProcessing.isExpressionNode(slot.componentOptions.tag)) {
-            countExpression += 1
-          } else {
-            throw new QtiValidationException('Node is not an Expression: "' + slot.componentOptions.tag + '"')
-          }
-        }
-      })
-      if (countExpression !== 2) {
-        throw new QtiValidationException('Must have exactly two Expression nodes')
-      }
-      // Perform extra semantic validations on the expressions
-      this.validateExpressions()
-      // All good.  Save off our children.
-      this.processChildren()
-    },
-
-    validateExpressions () {
-      this.$children.forEach((expression) => {
-        if (expression.getCardinality() !== 'single') {
-          throw new QtiValidationException('Expressions must be cardinality="single"')
-        }
-        if (!qtiProcessing.isBaseTypeNumeric(expression.getBaseType())) {
-          throw new QtiValidationException('Expressions must be a numeric base-type')
-        }
-      })
-    },
-
     processChildren () {
-      this.$children.forEach((expression) => {
-        this.expressions.push(expression)
+      const children = this.$.subTree.children[0].children
+
+      // Perform extra semantic validations on the expressions
+      this.validateExpressions(children)
+
+      children.forEach((expression) => {
+        if (expression.component === null) return
+        this.expressions.push(expression.component.proxy)
       })
     },
 
@@ -151,10 +139,23 @@ export default {
     }
   },
 
+  created () {
+    try {
+      this.validateChildren()
+    } catch (err) {
+      this.isQtiValid = false
+      if (err.name === 'QtiValidationException') {
+        throw new QtiValidationException(err.message)
+      } else {
+        throw new Error(err.message)
+      }
+    }
+  },
+
   mounted () {
     if (this.isQtiValid) {
       try {
-        this.validateChildren()
+        this.processChildren()
       } catch (err) {
         this.isQtiValid = false
         throw new QtiValidationException(err.message)
