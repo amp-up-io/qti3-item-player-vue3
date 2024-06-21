cd4ffd0eb3919bfc37d4fc965dba1c3d23ca5e5d
paulgrudnitski <paul.grudnitski@gmail.com>
2024-06-21 06:03:12 -0700

    Fix comment bug in QtiOrdered

--- src/components/qti/processing/expressions/QtiOrdered.vue
+++ src/components/qti/processing/expressions/QtiOrdered.vue
@@ -93,6 +93,7 @@ export default {
       this.validateExpressions(children)
 
       children.forEach((expression) => {
+        if (expression.component === null) return
         this.expressions.push(expression.component.proxy)
       })
     },
@@ -100,6 +101,7 @@ export default {
     validateExpressions (expressions) {
       let baseType = null
       expressions.forEach((expression) => {
+        if (expression.component === null) return
         const node = expression.component.proxy
         const cardinality = node.getCardinality()
         if ((cardinality !== 'single') && (cardinality !== 'ordered')) {
