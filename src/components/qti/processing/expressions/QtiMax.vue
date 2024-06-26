3395a067d2e10d5d7a4ddc77c3e6aef767d2c268
paulgrudnitski <paul.grudnitski@gmail.com>
2024-06-26 14:23:37 -0700

    Fix QtiMax and QtiMin default baseType bug.
    should be integer ... unless there is at least 1 float.

--- src/components/qti/processing/expressions/QtiMax.vue
+++ src/components/qti/processing/expressions/QtiMax.vue
@@ -30,7 +30,7 @@ export default {
   data () {
     return {
       value: null,
-      valueBaseType: null,
+      valueBaseType: 'integer',
       valueCardinality: 'single',
       expressions: [],
       isQtiValid: true
