0861edcc2090c87e5c2349806bc011c03f5204a9
paulgrudnitski <paul.grudnitski@gmail.com>
2024-07-26 18:20:04 -0700

    Prep for vite module build

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Qti3Player from './components/Qti3Player.vue'
+
+export default {
+  install: (app) => {
+    app.component('Qti3Player', Qti3Player)
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+/* global __dirname */
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'Qti3Player',
+      // the proper extensions will be added
+      fileName: 'qti3-item-player-vue3',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [
     vue()
   ],
