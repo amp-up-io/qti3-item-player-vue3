ead9a2a5ee6d35d1a76e6a25029f277b48db6962
Martin Stanojevic <stanojevic.martin@gmail.com>
2025-06-21 13:18:14 +0200

    Fix non-existent function call

--- src/components/qti/QtiAssessmentItem.vue
+++ src/components/qti/QtiAssessmentItem.vue
@@ -405,7 +405,7 @@ export default {
       if (!store.getItemContextSessionControl().getShowSolution()) return
 
       // NOOP if Item lifecycle is not 'solution'
-      if (store.getItemLifecycleState() !== 'solution') return
+      if (store.getItemLifecycleStatus() !== 'solution') return
 
       store.getInteractions().forEach((interaction) => {
         if (interaction.node.showSolution) {
